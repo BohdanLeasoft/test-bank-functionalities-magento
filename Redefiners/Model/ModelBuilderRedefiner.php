acdb08f8ad888c59000511cdb0d596c2e0996039
Bohdan <bohdan.shevchenko2@nure.ua>
2021-11-22 12:37:05 +0200

    Added bank-func

---
+++ Redefiners/Model/ModelBuilderRedefiner.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace GingerPay\Payment\Redefiners\Model;
+
+require_once __DIR__.'/../../Model/Builders/LibraryConfigProvider.php';
+
+use GingerPay\Payment\Model\Builders\LibraryConfigProvider;
+
+class ModelBuilderRedefiner extends LibraryConfigProvider
+{
+
+}
